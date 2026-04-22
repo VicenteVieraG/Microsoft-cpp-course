7abf2fcb04c58da172b09c745aea1ced88a10ae8
Vicente Javier Viera Guizar <vicentejvg@outlook.com>
2026-04-21 18:10:48 -0600

    Update CMake configuration and add module_1 with basic functionality

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <config.hpp>
+#include <module_1.hpp>
 
 int main(int argc, char** argv){
     std::cout<<project_name<<std::endl;
     std::cout<<project_version<<std::endl;
+    std::cout<<TEST<<std::endl;
 
     return 0;
 }
